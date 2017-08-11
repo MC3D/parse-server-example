08e2a01a91f5e0dc651ee77ca8b6c3b62dd6155b
MC3D <m.chapman.home@gmail.com>
2017-08-11 09:54:10 -0400

    added mars get endpoint

--- index.js
+++ index.js
@@ -46,6 +46,19 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
+const BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
+const API_KEY = 'XiPVohbJ1czo1N4Czgvs87NBaWCJMwr4V6P7Q8M4';
+let imageUrl = `${BASE_URL}Opportunity/photos?sol=2000&camera=RHAZ (Rear Hazard)&api_key=${API_KEY}`;
+
+app.get('/mars', function(req, res) {
+  console.log('mars request', req);
+  request(imageUrl, function(error, response, body) {
+    if(!error && reponse.statusCode === 200) {
+      res.send(body);
+    }
+  })
+});
+
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
